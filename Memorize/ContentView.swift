ae87f316f78a0d4ff6008a22f36f308a04462a8e
levinjoey@gmail.com <levinjoey@gmail.com>
2025-02-02 19:09:33 +0200

    Created CardView Added ForEach

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,10 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+  let emojis = ["🐶","🐱","🐭","🐹"]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+      HStack {
+        ForEach(emojis.indices, id: \.self) { index in
+          CardView(content: emojis[index])
+        }
+      }
+      .foregroundColor(.orange)
+      .padding()
     }
+}
+
+struct CardView: View {
+  let content: String
+  @State var isFaceUp = true
+  
+  var body: some View {
+    ZStack {
+      let base = RoundedRectangle(cornerRadius: 12)
+      if isFaceUp {
+        base.fill(.white)
+        base.strokeBorder(lineWidth: 2)
+        Text(content).font(.largeTitle)
+      }
+      else {
+        base.fill()
+      }
+    }.onTapGesture {
+      isFaceUp.toggle()
+    }
+  }
 }
 
 

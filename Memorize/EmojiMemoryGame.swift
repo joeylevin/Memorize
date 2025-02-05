be2caced28bbf176a4e1644e112c0d1bfd9c7de0
levinjoey@gmail.com <levinjoey@gmail.com>
2025-02-05 15:28:29 +0200

    Formatting fixes

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,8 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by joey levin on 2/5/25.
+//
+
+import Foundation
---
+++ Memorize/MemoryGame.swift
@@ -0,0 +1,22 @@
+//
+//  MemorizeGame.swift
+//  Memorize
+//
+//  Created by joey levin on 2/5/25.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    var cards: Array<Card>
+    
+    func choose(card: Card) {
+        
+    }
+    
+    struct Card {
+        var isFaceUp: Bool
+        var isMatched: Bool
+        var content: CardContent
+    }
+}

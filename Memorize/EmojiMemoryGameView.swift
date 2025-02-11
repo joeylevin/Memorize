6820dfda0ed9e87e02637a278555d1a9cba9fa31
levinjoey@gmail.com <levinjoey@gmail.com>
2025-02-11 15:37:57 +0200

    Lecture 6 completed. Added AspectVGrid

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -9,14 +9,13 @@ import SwiftUI
 
 struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
+    private let aspectRatio: CGFloat = 2/3
     
     var body: some View {
         VStack {
             Text("Memorize: \(viewModel.theme.name)").font(.largeTitle)
-            ScrollView {
-                cards
-                    .animation(.default, value: viewModel.cards)
-            }
+            cards
+            .animation(.default, value: viewModel.cards)
             Spacer()
             HStack {
                 Button("New Game") {
@@ -29,16 +28,13 @@ struct EmojiMemoryGameView: View {
         .padding()
     }
     
-    var cards: some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 100), spacing: 0)]) {
-            ForEach(viewModel.cards) { card in
-                CardView(card: card, theme: viewModel.theme, color: viewModel.themeColor)
-                    .aspectRatio(2/3, contentMode: .fit)
-                    .padding(4)
-                    .onTapGesture {
-                        viewModel.choose(card)
-                    }
-            }
+    private var cards: some View {
+        AspectVGrid(viewModel.cards, aspectRatio: aspectRatio) { card in
+            CardView(card: card, theme: viewModel.theme, color: viewModel.themeColor)
+                .padding(4)
+                .onTapGesture {
+                    viewModel.choose(card)
+                }
         }
     }
 }

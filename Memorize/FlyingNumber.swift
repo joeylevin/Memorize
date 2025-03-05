0012cb72bbf334c352afb25fa26de5e95f61ebce
levinjoey@gmail.com <levinjoey@gmail.com>
2025-03-05 13:57:02 +0200

    Flying number working.

--- Memorize/FlyingNumber.swift
+++ Memorize/FlyingNumber.swift
@@ -10,10 +10,29 @@ import SwiftUI
 struct FlyingNumber: View {
     let number: Int
     
+    @State private var offset: CGFloat = 0
+    
     var body: some View {
         if (number != 0) {
-            Text(number, format: .number)
+            Text(number, format: .number.sign(strategy: .always()))
+                .font(.largeTitle)
+                .foregroundColor(number < 0 ? .red : .green)
+                .shadow(color: .black, radius: 1.5, x: 1, y: 1)
+                .offset(x: 0, y: offset)
+                .opacity(offset != 0 ? 0 : 1)
+                .onAppear {
+                    withAnimation(.easeIn(duration: Constants.animationDuration)) {
+                        offset = number < 0 ? Constants.newOffset: -1*Constants.newOffset
+                    }
+                }
+                .onDisappear {
+                    offset = 0
+                }
         }
+    }
+    private struct Constants {
+        static let newOffset: CGFloat = 200
+        static let animationDuration: TimeInterval = 1.5
     }
 }
 
